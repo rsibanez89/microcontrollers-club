304c039d3c890220b7e3a6cff1447aeafb15c15b
rsibanez89 <rsibanez89@gmail.com>
2020-03-11 19:27:30 +1100

    Dont wait

--- tank-interface/app.py
+++ tank-interface/app.py
@@ -2,21 +2,18 @@ from flask import Flask, render_template, request, url_for, jsonify, Response
 import serial
 import sys
 import socket
-import time
-
-# Getting IP Address
-hostname = socket.gethostname()    
-ip = "ip:" + socket.gethostbyname(hostname + ".local")
 
 ser = serial.Serial("/dev/ttyACM0",9600)
 ser.baudrate=9600
-time.sleep(15) # Wait until Arduino is Connected.
-ser.write(ip.encode())
 
 app = Flask(__name__)
 
 @app.route('/', methods=['GET'])
 def home():
+    # Getting IP Address
+    hostname = socket.gethostname()    
+    ip = "ip:" + socket.gethostbyname(hostname + ".local")
+    ser.write(ip.encode())
     return render_template('index.html')
 
 @app.route('/forward', methods=['GET'])
