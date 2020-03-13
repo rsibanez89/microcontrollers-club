afff4ec11ffbac97c601ac23273ff87c4c97a6ae
rsibanez89 <rsibanez89@gmail.com>
2020-03-13 16:51:24 +1100

    wait until internet connection

--- deploy.sh
+++ deploy.sh
@@ -1,6 +1,11 @@
 # first time
 # git clone https://github.com/rsibanez89/microcontrollers-club.git
 
+while :; do
+   ping -c 1 -w 3 8.8.8.8; echo $? > /tmp/ping.status
+   sleep 1
+done
+
 cd /home/pi/microcontrollers-club
 git pull
 
